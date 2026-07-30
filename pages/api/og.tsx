58f585563811604dd2d0f1bf4e2956f8338a6f2f
Jovceo <194472112+Jovceo@users.noreply.github.com>
2026-07-30 09:30:19 +0800

    1

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -131,6 +131,9 @@ export default function handler(req: NextRequest) {
             {
                 width: 1200,
                 height: 630,
+                headers: {
+                    'Cache-Control': 'public, max-age=86400, stale-while-revalidate=604800',
+                },
             },
         );
     } catch (e: unknown) {
